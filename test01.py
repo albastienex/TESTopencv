a21637d371adaabc4349833590270adcc5c28dc0
albastienex <a980364@gmail.com>
2021-01-28 12:04:18 +0800

    plt

--- test01.py
+++ test01.py
@@ -1,16 +1,20 @@
 import cv2
 import numpy as np 
+from matplotlib import pyplot as plt  #matplotlib初始設定
 img = cv2.imread('test.jpg',cv2.IMREAD_GRAYSCALE)  
 
 # test.jpg後面直接輸入0會顯示黑白1會顯示彩色
 
 print(img)    #從灰階叫出img可以知道圖片的大小
               #從彩色叫出img可以知道RGB組成   
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])
+plt.show()
+# cv2.namedWindow('test',cv2.WINDOW_NORMAL)  #顯示的圖檔初始設定為無法更改大小，可用此語法更改
 
-cv2.namedWindow('test',cv2.WINDOW_NORMAL)
-cv2.imshow('test', img) #顯示圖檔
+# cv2.imshow('test', img) #顯示圖檔
 
-cv2.imwrite('test1.jpg', img)  #跑出圖檔後以test1的檔名存取
+# cv2.imwrite('test1.jpg', img)  #跑出圖檔後以test1的檔名存取
 
 cv2.waitKey(0)     
 #這裡 cv2.waitKey 函數是用來等待與讀取使用者按下的按鍵
